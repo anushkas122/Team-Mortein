7a884138209a2af20174be20c0d8d98f476d7520
Minttyy <sabrina_teoh@hotmail.com>
2020-08-08 17:47:17 +1200

    add Search Bar

--- components/layout.js
+++ components/layout.js
@@ -1,22 +1,18 @@
-import Head from 'next/head'
-import Header from './header'
-import Footer from './footer'
-
+// import Head from "next/head";
+// import Header from "./header";
+import Footer from "./footer";
+import SideBar from "./sidebar";
 
 function Layout({ user, loading = false, children }) {
   return (
     <>
-      <Head>
-        <title>WDCC - App Template</title>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
-        />
-      </Head>
+      <div className="left">
+        <SideBar />
+      </div>
 
-      <Header user={user} loading={loading} />
-      <main className='container mx-auto'>{children}</main>
-      <Footer user={user} loading={loading} /> 
+      {/* <Header user={user} loading={loading} /> */}
+      <main className="container mx-auto">{children}</main>
+      <Footer user={user} loading={loading} />
     </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles.css'
-function App ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles.css";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-export default App
+export default App;
