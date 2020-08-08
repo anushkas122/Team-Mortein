638ac942630fbc1dfbb3dc0cf53de25cbabd0a77
Minttyy <sabrina_teoh@hotmail.com>
2020-08-08 19:03:44 +1200

    combine sidebar and card content, header and footer together

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Head from 'next/head'
-import Header from './header'
-import ClubCard from './ClubCard'
+import React from "react";
+import Head from "next/head";
+import Header from "./header";
+import ClubCard from "./ClubCard";
 import Footer from "./footer";
 import SideBar from "./sidebar";
 
-function Layout ({user, loading = false, children }) {
+function Layout({ user, loading = false, children }) {
   return (
     <>
       <div className="left">
@@ -14,10 +14,9 @@ function Layout ({user, loading = false, children }) {
 
       <Header user={user} loading={loading} />
       <main className="container mx-auto flex-1">{children}</main>
-      <ClubCard/>
       <Footer user={user} loading={loading} />
     </>
   );
 }
 
-export default Layout
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,54 @@
-import fetch from 'node-fetch'
-// import Layout from '../components/layout'
-import Categories from '../components/categories'
+import fetch from "node-fetch";
+import Layout from "../components/layout";
+import Categories from "../components/categories";
+import Footer from "../components/footer";
+import SideBar from "../components/sidebar";
+import Header from "../components/header";
 
-function Home () {
+function Home() {
   return (
-    <div>
-      <div className = "h-screen flex flex-col"> 
-      <Layout user={user} loading={loading} >
-        <h1>Page heading</h1>
-        <p>Interesting content here</p>
+    <div className="h-screen flex flex-col">
+      <SideBar />
+      {/* {loading && <p>Loading login info...</p>}
+          {!loading && !user && (
+            <>
+              <p>
+                To view the dashboard <a href="/api/login">Login</a>
+              </p>
+            </>
+          )}
+          {user && (
+            <>
+              <ProfileCard user={user}>
+                <button className="btn-blue" onClick={handleClick}>
+                  Event
+                </button>
+              </ProfileCard>
+              <h2>Reports</h2>
+              <LinkA href="/report">Report - useEffect</LinkA>
+              <LinkA href="/reportSSR">Report - SSR</LinkA>
+              <LinkA href="/reportSWR">Report - SWR</LinkA>
+            </>
+          )}
 
-        {loading && <p>Loading login info...</p>}
-        {!loading && !user && (
-          <>
-            <p>
-              To view the dashboard <a href="/api/login">Login</a>
-            </p>
-          </>
-        )}
-        {user && (
-          <>
-            <ProfileCard user={user}>
-              <button className="btn-blue" onClick={handleClick}>
-                Event
-              </button>
-            </ProfileCard>
-            <h2>Reports</h2>
-            <LinkA href="/report">Report - useEffect</LinkA>
-            <LinkA href="/reportSSR">Report - SSR</LinkA>
-            <LinkA href="/reportSWR">Report - SWR</LinkA>
-          </>
-        )}
+          <Button></Button> */}
+      <Header />
+      <main className="container mx-auto flex-1">
+        <h1>SPORTS</h1>
+        <Categories category={"Sport"} />
+        <h1>ACADEMIC</h1>
+        <Categories category={"Academic"} />
+        <h1>RELIGIOUS</h1>
+        <Categories category={"Religious"} />
+        <h1>CULTURE</h1>
+        <Categories category={"Cultural"} />
+        <h1>CAUSES</h1>
+        <Categories category={"Causes"} />
+      </main>
 
-        <Button></Button>
-      </Layout>
-      </div>
-    );
-
-      <h1>SPORTS</h1>
-      <Categories category={"Sport"}/>
-      <h1>ACADEMIC</h1>
-      <Categories category={"Academic"}/>
-      <h1>RELIGIOUS</h1>
-      <Categories category={"Religious"}/>
-      <h1>CULTURE</h1>
-      <Categories category={"Cultural"}/>
-      <h1>CAUSES</h1>
-      <Categories category={"Causes"}/>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Home;
