aa785eeba8a00bed8ed93d53f10c3ddad8c91b93
Anushka Siriwardane <anushkas122@gmail.com>
2020-08-08 14:41:45 +1200

    basic layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,8 @@
+import React from 'react'
 import Head from 'next/head'
 import Header from './header'
+import Footer from './footer'
+import ClubCard from '.ClubCard'
 
 function Layout ({ user, loading = false, children }) {
   return (
@@ -8,12 +11,45 @@ function Layout ({ user, loading = false, children }) {
         <title>WDCC - App Template</title>
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic' />
       </Head>
-
+    
       <Header user={user} loading={loading} />
-      <main className='container mx-auto'>{children}</main>
-
-    </>
+      SPORTS
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      ACADEMIC
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      RELIGIOUS
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      CULTURE
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      CAUSES   
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <ClubCard data={name=clubName, picLink=pictureResource, blurb=blurb}> </ClubCard>;
+      <Footer/>
+      </>
   )
 }
 
-export default Layout
+export default Layout;
